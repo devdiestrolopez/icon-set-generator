4967c429f361472c258a4f705e557d6a355202c3
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2025-08-17 16:43:05 -0400

    fix: rollback to public visibility for gradle extension

--- iconset-generator-plugin/src/main/kotlin/es/devdiestrolopez/iconsetgenerator/plugin/IconSetExtension.kt
+++ iconset-generator-plugin/src/main/kotlin/es/devdiestrolopez/iconsetgenerator/plugin/IconSetExtension.kt
@@ -2,7 +2,7 @@ package es.devdiestrolopez.iconsetgenerator.plugin
 
 import org.gradle.api.provider.Property
 
-internal interface IconSetExtension {
+interface IconSetExtension {
 
     val outputPackage: Property<String>
     val fileName: Property<String>
