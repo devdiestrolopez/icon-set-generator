a2207a6f6a87fa6739f93378a93571d9f5209ab7
Carlos Diestro <48551464+devdiestrolopez@users.noreply.github.com>
2025-09-03 12:43:51 -0400

    feat: added icons to test

---
+++ app/src/icons/kotlin/MaterialIcons.kt
@@ -0,0 +1,11 @@
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.rounded.Search
+
+val HomeFilled = Icons.Filled.Home
+val HomeOutlined = Icons.Outlined.Home
+val Search = Icons.Rounded.Search
+val Back = Icons.AutoMirrored.Rounded.ArrowBack
